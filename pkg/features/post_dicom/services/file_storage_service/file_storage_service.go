c01c76223e99732860057bddf7fc574bd3078cc6
whkelvin <whkelvin.hsu@gmail.com>
2023-03-18 19:15:46 -0300

    added in file storage service

---
+++ pkg/features/post_dicom/services/file_storage_service/file_storage_service.go
@@ -0,0 +1,38 @@
+package file_storage_service
+
+import (
+  "errors"
+  "io"
+  "mime/multipart"
+  "os"
+  "github.com/google/uuid"
+  "fmt"
+  "path/filepath"
+)
+
+func SaveFile(file *multipart.FileHeader) (string, error) {
+  src, err := file.Open()
+  if(err!= nil){
+    return "", errors.New("saving file failed.")
+  }
+  defer src.Close()
+  uuid := uuid.New()
+  
+  dstPath := fmt.Sprintf("/home/whkelvin/Projects/golang/dicom_api/assets/uploaded/dcm/%v.dcm", uuid)
+  dst, err := os.Create(dstPath)
+  if err != nil {
+    return "", errors.New("saving file failed.")
+  }
+  defer dst.Close()
+
+  if _, err = io.Copy(dst, src); err != nil {
+    return "", errors.New("saving file failed.")
+  }
+
+  absPath, err := filepath.Abs(dstPath)
+  if err != nil {
+    return "", errors.New("saving file failed.")
+  }
+
+  return absPath, nil
+}
